8d3d60e53bec2b7ef43137bdc9a9941de262c808
frank-job <Jobf0837@gmail.com>
2026-05-11 14:54:46 +0300

    db

--- src/models/db.js
+++ src/models/db.js
@@ -13,10 +13,7 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    // ssl: true
-     ssl: {
-    rejectUnauthorized: false // <--- THIS IS THE FIX
-     }
+    ssl: true
 });
 
 /**
