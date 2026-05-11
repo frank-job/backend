c91999c58f476e927862b21556948c062f2ab471
frank-job <Jobf0837@gmail.com>
2026-05-11 15:20:29 +0300

    db again

--- src/models/db.js
+++ src/models/db.js
@@ -1,4 +1,9 @@
 import { Pool } from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 /**
  * Connection pool for PostgreSQL database.
@@ -13,7 +18,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+    rejectUnauthorized: false // <--- THIS LINE IS THE FIX
+  }
 });
 
 /**
