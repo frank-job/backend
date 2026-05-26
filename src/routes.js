162441dfb31ccfd576c0043d280920c67e7d0fb0
frank-job <jobf0837@gmail.com>
2026-05-26 11:11:19 +0300

    Repeat the process: Inserting New Service Projects

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { showOrganizationDetailsPage, showOrganizationsPage,  } from './controllers/organizations.js';
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js';
 import { showHomePage } from './controllers/index.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 import { showNewOrganizationForm } from './controllers/organizations.js';
-import { processNewOrganizationForm } from './controllers/organizations.js';
+import { processNewOrganizationForm, organizationValidation,showEditOrganizationForm,processEditOrganizationForm } from './controllers/organizations.js';
 
 
 
@@ -19,9 +19,18 @@ router.get('/organizations', showOrganizationsPage);
 router.get('/categories', showCategoriesPage);
 // Route for new organization page
 router.get('/new-organization', showNewOrganizationForm);
-
 // Route to handle new organization form submission
-router.post('/new-organization', processNewOrganizationForm);
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
 router.get('/test-error', testErrorPage);
+// Route to display the edit organization form
+router.get('/edit-organization/:id', showEditOrganizationForm);
+// Route to handle the edit organization form submission
+// Route to handle the edit organization form submission
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 
+// Route for new project page
+router.get('/new-project', showNewProjectForm);
+
+// Route to handle new project form submission
+router.post('/new-project', processNewProjectForm, projectValidation);
 export default router;
